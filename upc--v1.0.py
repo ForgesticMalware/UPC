3643a0081dc072dd695615b1d0e59871f9b43f6a
Forgestics_ <forgesticmalware@gmail.com>
2025-03-21 21:28:44 -0700

    Usable Program Compressor -- Version 1.0

---
+++ upc--v1.0.py
@@ -0,0 +1,37 @@
+def text(text):{
+    print(text)
+}
+def list(list_name, L1, L2, L3):{
+    print("new list", list_name, ":"),
+    print("1:"),
+    print(L1),
+    print("2:"),
+    print(L2),
+    print("3:"),
+    print(L3)
+}
+def check(Check):{
+    print("checking", A),
+    print(A, "= True")
+}
+import os
+def clear():{
+    os.system('cls')
+}
+import turtle
+def rect(size):
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.right(90)
+    for _ in range(4):
+        t.forward(size)
+        t.right(90)
+
+def help():{
+    print("go to https://github.com/ForgesticMalware/UPC/blob/main/Tutorial%3A%20The%20Basics for help")
+}
+def local(local):{
+    
+text("Usable Program Compressor, UPC -- version 1.0")
+
+
